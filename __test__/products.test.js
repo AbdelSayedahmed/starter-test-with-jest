9a748da44a5676cd0a94fcddb1e15ed5defcb97d
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-06-08 12:31:18 -0400

    done

--- __test__/products.test.js
+++ __test__/products.test.js
@@ -4,15 +4,15 @@ describe("getFullName()", () => {
   it("It should return an object of names of a person.", () => {
     const input = { names: { first: "Chelsea", surname: "Hernandez" } };
     const actual = getFullName(input);
-    const expected = "Chelsea Hernandez"
+    const expected = "Chelsea Hernandez";
     expect(actual).toEqual(expected);
   });
 });
 
 describe("getProductsPurchased()", () => {
-    it("It should return 'No products purchased.' if no products are purchased", () => {
-        const input = { purchased: [] };
-        const actual = getProductsPurchased(input);
-        expect(actual).toEqual("No products purchased.");
-      });      
+  it("It should return 'No products purchased.' if no products are purchased", () => {
+    const input = { purchased: [] };
+    const actual = getProductsPurchased(input);
+    expect(actual).toEqual("No products purchased.");
+  });
 });
